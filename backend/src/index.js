57cbc270df914983f4ff5627de88e3b5b5c59a9a
rAhul6280 <rrahul62808@gmail.com>
2026-06-02 12:56:18 +0530

    refactor: remove server listening log and streamline connection message

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,6 @@ import connectDB from './db/mongodb.js'
  connectDB().then(
    ()=>{
     const port = process.env.PORT || 5000
-    app.listen(port,()=>{
-    console.log(` ⚙ Server is running on port ${port} ⚙ `)
-    })
+   console.log(`App is running on port ${port}`);
 }
 ).catch((err)=>console.log("Error: ",err))
