073947e04004bcca4fd91fc010c0db50f3a321af
rAhul6280 <rrahul62808@gmail.com>
2026-06-02 13:47:44 +0530

    fix: update root path in router configuration and add Vercel configuration file

--- frontend/src/routes/app.routes.jsx
+++ frontend/src/routes/app.routes.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
+        path: "",
         element: (
           <AuthLayout authenticated={true}>
             {" "}
